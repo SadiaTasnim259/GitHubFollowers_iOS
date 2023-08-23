d441f5c8a5fc4cb529a90296ce9e37c079f665c4
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-08-23 11:40:28 +0600

    custom UItextField, UIButton added and image view added and given constrain to them into searchViewController

--- GitHubFollowers/SearchViewController.swift
+++ GitHubFollowers/SearchViewController.swift
@@ -8,13 +8,34 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    let logoImageView = UIImageView()
+    let usernameTextField = GitHubFollowerTextField()
+    let callToActionButton = GitHubFollowerButton(backgroundColor: .systemGreen, title: "Get Followers")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .systemCyan
+        view.backgroundColor = .systemBackground
+        configureLogoImageView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
     }
 
-
+    func configureLogoImageView(){
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "GitHub-logo")
+        logoImageView.contentMode = .scaleToFill
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 360),
+            logoImageView.widthAnchor.constraint(equalToConstant: 360)
+        ])
+    }
 }
 
