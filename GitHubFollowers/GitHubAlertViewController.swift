5ec6cefda10608d482e8ca8279800e25f9b059ec
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-08-25 21:43:48 +0600

    custom titleLabel and MessageLabel is created in alertViewController also constrain added

--- GitHubFollowers/GitHubAlertViewController.swift
+++ GitHubFollowers/GitHubAlertViewController.swift
@@ -8,22 +8,65 @@
 import UIKit
 
 class GitHubAlertViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    let containerView = UIView()
+    let titleLabel = GithubFollowerTitleLabel(textAlignment: .center, fontSize: 20)
+    let messageLabel = GitHubFollowerBodyLabel(textAlignment: .center)
+    let actionButton = GitHubFollowerButton(backgroundColor: .systemMint, title: "ok")
+    
+    var alertTitle: String?
+    var message: String?
+    var buttonTitle: String?
+    
+    let padding:CGFloat = 20
+    
+    init(title: String, message:String, buttonTitle: String){
+        super.init(nibName: nil, bundle: nil)
+        self.alertTitle = title
+        self.message = message
+        self.buttonTitle = buttonTitle
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+        configureContailerView()
+    }
+    
+    func configureContailerView(){
+        view.addSubview(containerView)
+        containerView.backgroundColor = .systemBackground
+        containerView.layer.cornerRadius = 16
+        containerView.layer.borderWidth = 2
+        containerView.layer.borderColor = UIColor.white.cgColor
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            containerView.widthAnchor.constraint(equalToConstant: 280),
+            containerView.heightAnchor.constraint(equalToConstant: 220)
+        ])
+    }
+    
+    func configureTitleLabel(){
+        containerView.addSubview(titleLabel)
+        titleLabel.text = alertTitle ?? "Something went wrong"
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: padding),
+            titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: padding),
+            titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
+            titleLabel.heightAnchor.constraint(equalToConstant: 28)
+        ])
+    }
 
+    func configureActionButton(){
+        containerView.addSubview(actionButton)
+        
+    }
 }
