7d551f271f5ffcae14b3105f8c5af346d611bb75
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-08-28 07:13:47 +0600

    follower and user model created

--- GitHubFollowers/Model/User.swift
+++ GitHubFollowers/Model/User.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct User: Codable{
+    var login: String
+    var avatarUrl : String
+    var name: String?
+    var location: String?
+    var bio: String
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var followers: Int
+    var following: Int
+    var createAt: String
+}
