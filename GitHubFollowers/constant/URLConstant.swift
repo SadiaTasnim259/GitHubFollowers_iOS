1193660c8d4deea90e6f5254ebbb159350d32505
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-08-28 09:36:52 +0600

    network manager created and decoded data

---
+++ GitHubFollowers/constant/URLConstant.swift
@@ -0,0 +1,12 @@
+//
+//  URLConstant.swift
+//  GitHubFollowers
+//
+//  Created by Sadia on 28/8/23.
+//
+
+import Foundation
+
+enum API{
+    static let BASE_URL = "https://api.github.com/users/"
+}
