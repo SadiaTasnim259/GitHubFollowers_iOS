a8d738177bbd1777f3fa4f59df9eb3ff5ab43a0d
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-08-30 22:24:34 +0600

    collectionViewCell created and custom identifier given

---
+++ GitHubFollowers/Custom Views/Cells/FollowerCell.swift
@@ -0,0 +1,12 @@
+//
+//  FollowerCell.swift
+//  GitHubFollowers
+//
+//  Created by Sadia on 30/8/23.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    static let reuseID = "FollowerCell"
+}
