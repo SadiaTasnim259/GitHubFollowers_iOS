1838e4298c7406bb75fee5d72f43c9275c878c5f
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-08-31 23:05:29 +0600

    updating configureCollectionView() from UICollectionViewFlowLayout() to createThreeColumnFlowLayout()

--- GitHubFollowers/Screens/FollowerListViewController.swift
+++ GitHubFollowers/Screens/FollowerListViewController.swift
@@ -50,10 +50,16 @@ class FollowerListViewController: UIViewController {
     
     
     func configureCollectionView(){
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemPink
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
+    
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout{
+        
+        return UICollectionViewFlowLayout()
+    }
 
 }
+
